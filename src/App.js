45ba7e8a3cefb7c2b5f185db67ea7938af382fb6
0qsenxx <semen.antipyuk@gmail.com>
2024-07-27 12:34:53 +0300

    Updates

--- src/App.js
+++
@@ -1,6 +0,0 @@
-import { Main } from "./pages/main/Main";
-function App() {
-  return <Main />;
-}
-
-export default App;
--- src/components/Logo/Logo.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Logo = styled.h2`
-  color: rgb(1, 25, 54);
-  font-size: 35px;
-  font-weight: 400;
-  line-height: 120%;
-`;
